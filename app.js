788173a1a3c4f906688a1755745db9084c61d58c
NhungTranPY <nhungtranpy91@gmail.com>
2023-02-27 21:40:35 +0700

    429. sessionStorage

--- app.js
+++ app.js
@@ -9,12 +9,12 @@ const user = {
 }
 
 storeBtn.addEventListener('click', () => {
-    localStorage.setItem('uid', userId)
+    sessionStorage.setItem('uid', userId)
     localStorage.setItem('user', JSON.stringify(user))
 })
 
 retrBtn.addEventListener('click', () => {
-    const extractedId = localStorage.getItem('uid')
+    const extractedId = sessionStorage.getItem('uid')
     const extractedUser = JSON.parse(localStorage.getItem('user'))
     console.log(extractedUser);
     if (extractedId) {
